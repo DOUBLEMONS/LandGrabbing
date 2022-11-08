d36bef47494d3e452d8dff2ec204975ca5424344
김은총 <monmon867867@gmail.com>
2022-11-09 00:22:51 +0900

    플레이어간 충돌 효과 구현

--- Assets/김은총/Scripts/Player/Player2_Move.cs
+++ Assets/김은총/Scripts/Player/Player2_Move.cs
@@ -6,6 +6,7 @@ public class Player2_Move : MonoBehaviour
 {
     public float MoveSpeed = 5f;
     public Transform MovePoint;
+    public float Radius;
 
     public LayerMask WhatStopMovement;
 
@@ -22,37 +23,38 @@ public class Player2_Move : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, MovePoint.position, MoveSpeed * Time.deltaTime);
 
-            if (Vector3.Distance(transform.position, MovePoint.position) <= .05f)
+            if (Vector3.Distance(transform.position, MovePoint.position) <= 0.05f)
             {
-                if (!Physics2D.OverlapCircle(MovePoint.position + new Vector3(Input.GetAxisRaw("LeftRightArrow"), 0f, 0f), 0.25f, WhatStopMovement))
+                if (Mathf.Abs(Input.GetAxisRaw("LeftRightArrow")) == 1f)
                 {
-                    if (Mathf.Abs(Input.GetAxisRaw("LeftRightArrow")) == 1f)
+                    if (!Physics2D.OverlapCircle(MovePoint.position + new Vector3(Input.GetAxisRaw("LeftRightArrow") , 0f, 0f), Radius, WhatStopMovement))
                     {
-                        if (Input.GetKey(KeyCode.LeftArrow)) 
-                        {
-                            MovePoint.position -= new Vector3(1.0f, 0.0f, 0.0f);
-                        }
-                        if (Input.GetKey(KeyCode.RightArrow)) 
-                        {
-                            MovePoint.position += new Vector3(1.0f, 0.0f, 0.0f);
-                        }
+                        MovePoint.position += new Vector3(Input.GetAxisRaw("LeftRightArrow"), 0.0f, 0.0f);
                     }
                 }
-                else if (!Physics2D.OverlapCircle(MovePoint.position + new Vector3(0f, Input.GetAxisRaw("UpDownArrow"), 0f), 0.25f, WhatStopMovement))
+                else if (Mathf.Abs(Input.GetAxisRaw("UpDownArrow")) == 1f)
                 {
-                    if (Mathf.Abs(Input.GetAxisRaw("UpDownArrow")) == 1f)
+                    if (!Physics2D.OverlapCircle(MovePoint.position + new Vector3(0f, Input.GetAxisRaw("UpDownArrow"), 0f), Radius, WhatStopMovement))
                     {
-                        if (Input.GetKey(KeyCode.UpArrow))
-                        {
-                            MovePoint.position += new Vector3(0.0f, 1.0f, 0.0f);
-                        }
-                        if (Input.GetKey(KeyCode.DownArrow))
-                        {
-                            MovePoint.position -= new Vector3(0.0f, 1.0f, 0.0f);
-                        }
+                        MovePoint.position += new Vector3(0.0f, Input.GetAxisRaw("UpDownArrow"), 0.0f);
                     }
                 }
             }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player1"))
+        {
+            Debug.Log("Player1");
+            MoveSpeed = 6.25f;
+            Invoke("Reset", 0.5f);
+        }
+    }
+
+    private void Reset()
+    {
+        MoveSpeed = 10f;
     }
 }
